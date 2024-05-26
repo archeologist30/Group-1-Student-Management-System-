bffbc04387c6f0ed1a8dd6c55f351fe59bf01f26
Elihu France Caalim <159905595+Paris1229@users.noreply.github.com>
2024-05-26 13:33:02 +0800

    Add files via upload

---
+++ StudentManagement/src/Student/GradingSystem.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Student;
+
+import java.beans.Statement;
+
+public class GradingSystem {
+    /* Connection con = MyConnection.getConnection();
+           PreparedStatement ps;
+    public int getMax(){
+        int id = 0;
+        Statement st;
+        try {
+            st = con.createStatement(); 
+            ResultSet rs = st.executeQuery("Select max(id) from GradingSystem");
+            while (rs.next()){
+                id = rs.getInt(1);
+    }
+        } catch (SQLException ex){
+                Logger.getLogger(GradingSystem.getName()).log(Level.SEVERE,null, ex);
+    
+            }
+                return id + 1;
+    }
+}   
+public boolean getDetails(int id){
+    try {
+        ps = con.prepareStatement("select * from student where id = ?" );
+        ps.setInt(1,id);
+        ResultSet rs =  ps.executeQuery();
+        if (rs.next()){
+            Home.
+        }
+    }
+    }*/
+} 
+
+    
+
+    
